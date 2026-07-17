cec697db91cc7392da3a1e5a1427802c2d3ab4b9
Prashantfr <prashantiwari2005@gmail.com>
2026-07-17 15:21:05 +0530

    studied about the lower and upper bound

---
+++ Binary_Search/Lower_bound.java
@@ -0,0 +1,27 @@
+//missed the = sign in the line 15 which resulted in the code for the upper bound
+
+
+package Binary_Search;
+
+class Solution {
+    public int Lower_bound(int[] nums, int x) {
+       int low = 0;
+       int high = nums.length-1;
+       int ans = nums.length;
+
+       while(low <= high){
+        int mid = low + (high-low)/2;
+
+        if(nums[mid]>=x){
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+       }
+       return ans;
+     }
+}
+
+
