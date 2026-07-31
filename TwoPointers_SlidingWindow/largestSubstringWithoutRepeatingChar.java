5e52479b70219f983d4f8b3c07645605b6a4454b
Prashantfr <prashantiwari2005@gmail.com>
2026-07-31 16:47:45 +0530

    Optimized the longest substring without repeating character problem to O(N) time complexity

--- TwoPointers_SlidingWindow/largestSubstringWithoutRepeatingChar.java
+++ TwoPointers_SlidingWindow/largestSubstringWithoutRepeatingChar.java
@@ -1,8 +1,7 @@
 //problem link => https://leetcode.com/problems/longest-substring-without-repeating-characters/
-//brute force with time complexity => O(N²)
-package TwoPointers_SlidingWindow;
 
-class Solution {
+//brute force with time complexity => O(N²) and space complexity => O(1).
+/*class Solution {
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
         int maxLen = 0;
@@ -18,6 +17,30 @@ class Solution {
                 int len = j-i+1;
                 maxLen = Math.max(maxLen,len);
             }
+        }
+        return maxLen;
+    }
+}*/
+
+
+//optimal solution with a time complexity => O(N) and space complexity => O(1).
+package TwoPointers_SlidingWindow;
+import java.util.Map;
+import java.util.HashMap;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Map< Character , Integer > lastSeen = new HashMap<>();
+        int maxLen = 0;
+        int left = 0;
+        for(int right=left; right<n; right++){
+            char ch = s.charAt(right);
+            if(lastSeen.containsKey(ch) && lastSeen.get(ch)>=left){
+                left = lastSeen.get(ch)+1;
+            }
+            lastSeen.put(ch,right);
+            int len = right-left+1;
+            maxLen = Math.max(len,maxLen);
         }
         return maxLen;
     }
