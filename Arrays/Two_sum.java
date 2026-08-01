3ad3185372ac3c90cc2e84d29ed1b54bc927618a
Prashantfr <prashantiwari2005@gmail.com>
2026-08-01 23:22:19 +0530

    revised how to optimize two sum problem from O(n^2) to O(n)

--- Arrays/Two_sum.java
+++ Arrays/Two_sum.java
@@ -1,4 +1,30 @@
-package Arrays;
+//Two Sum
+//Problem link: https://leetcode.com/problems/two-sum/
+
+//brute solution with time complexity of O(n^2) and space complexity O(1).
+/*
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // The outer loop picks the first number
+        for (int i = 0; i < nums.length; i++) {
+            
+            // The inner loop picks the second number (always starting after the first one)
+            for (int j = i + 1; j < nums.length; j++) {
+                
+                // Check if the pair adds up to the target
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j}; // Return their positions
+                }
+            }
+        }
+        
+        return new int[] {}; // Safety net
+    }
+}
+*/
+
+//optimal solution with time complexity of O(N) and space complexity O(n).
+
 import java.util.HashMap;
 import java.util.Map;
 class Solution {
