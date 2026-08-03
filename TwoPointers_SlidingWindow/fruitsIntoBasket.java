dea388241c3290ab600e788fd1db57daff201130
Prashantfr <prashantiwari2005@gmail.com>
2026-08-03 14:17:25 +0530

    optimized the longest subarrays with atmost 2 distinct numbers also called as fruits into basket problem to time complexity of O(n)

--- TwoPointers_SlidingWindow/fruitsIntoBasket.java
+++ TwoPointers_SlidingWindow/fruitsIntoBasket.java
@@ -2,8 +2,7 @@
 //brute force solution with time complexity of O(n^2) and space complexity O(1).
 
 package TwoPointers_SlidingWindow;
-import java.util.HashSet;
-
+/*import java.util.HashSet;
 class Solution {
     public int totalFruit(int[] fruits) {
         int n = fruits.length;
@@ -20,6 +19,37 @@ class Solution {
                 int len = j-i+1;
                 maxLen = Math.max(len,maxLen);
             }
+        }
+        return maxLen;
+    }
+}*/
+
+//optimal solution with time complexity of O(n) and space complexity O(1).
+
+import java.util.HashMap;
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        int left = 0;
+        int maxLen = 0;
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for(int right=0; right<n; right++){
+
+            map.put(fruits[right], map.getOrDefault(fruits[right] ,0) + 1);
+
+            while(map.size()>2){
+
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+
+                if(map.get(fruits[left]) == 0){
+                    map.remove(fruits[left]);
+                }
+                left++;
+            }
+
+            int len = right-left+1;
+            maxLen = Math.max(len, maxLen);
+
         }
         return maxLen;
     }
