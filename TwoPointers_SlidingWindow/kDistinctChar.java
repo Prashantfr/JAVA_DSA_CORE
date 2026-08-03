436d818247682bae451171cfbb6ebb1196169f9c
Prashantfr <prashantiwari2005@gmail.com>
2026-08-03 16:20:35 +0530

    optimized the longest Substring With At Most K Distinct Characters problem to time complexity of O(n) and space complexity of O(k)

--- TwoPointers_SlidingWindow/kDistinctChar.java
+++ TwoPointers_SlidingWindow/kDistinctChar.java
@@ -3,7 +3,7 @@
 //brute force solution with time complexity of O(n^2) and space complexity O(1).
 
 package TwoPointers_SlidingWindow;
-import java.util.HashSet;
+/*import java.util.HashSet;
 class Solution {
     public int kDistinctChar(String s, int k) {
         int n = s.length();
@@ -21,6 +21,36 @@ class Solution {
                     break;
                 }
             }
+        }
+        return maxLen;
+    }
+}*/
+
+
+//optimal solution with time complexity of O(n) and space complexity O(1) using a hashmap.
+
+import java.util.HashMap;
+class Solution {
+    public int kDistinctChar(String s, int k) {
+        int n = s.length();
+        int left = 0;
+        int maxLen = 0;
+        HashMap <Character, Integer> map = new HashMap<>();
+        for(int right=0; right<n; right++){
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while(map.size()>k){
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar)-1);
+
+                if(map.get(leftChar)==0){
+                    map.remove(leftChar);
+                }
+                left++;
+            }
+            int len = right-left+1;
+            maxLen = Math.max(len, maxLen);
         }
         return maxLen;
     }
