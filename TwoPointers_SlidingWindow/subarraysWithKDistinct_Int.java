652223d08a426b20eecfb5d5829dc8f3d0eb1eb2
Prashantfr <prashantiwari2005@gmail.com>
2026-08-03 17:48:07 +0530

    Optimized the subarrays With K Distinct integers problem to time complexity of O(n)

--- TwoPointers_SlidingWindow/subarraysWithKDistinct_Int.java
+++ TwoPointers_SlidingWindow/subarraysWithKDistinct_Int.java
@@ -4,7 +4,7 @@
 //brute force solution with time complexity of O(n^2) and space complexity O(k).
 package TwoPointers_SlidingWindow;
 
-import java.util.HashSet;
+/*import java.util.HashSet;
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
         int n = nums.length;
@@ -23,5 +23,34 @@ class Solution {
             }
         }
         return count;
+    }
+}*/
+
+//optimal solution with time complexity of O(n) and space complexity O(k).
+
+import java.util.HashMap;
+class Solution {
+    public int atMostK(int[] nums, int k) {
+        int n = nums.length;
+        int left = 0, count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int right=0; right<n; right++){
+            map.put(nums[right], map.getOrDefault(nums[right],0)+1);
+
+            while(map.size()>k){
+                map.put(nums[left], map.get(nums[left])-1);
+                if(map.get(nums[left])==0){
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            int len = right-left+1;
+            count += len;
+        }
+        return count;
+    }
+
+    public int subarraysWithKDistinct(int[] nums, int k){
+        return atMostK(nums,k) - atMostK(nums,k-1);
     }
 }
