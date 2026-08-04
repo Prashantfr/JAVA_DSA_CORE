2afa375344d306670311852686f10bd4a722e40c
Prashantfr <prashantiwari2005@gmail.com>
2026-08-04 12:32:09 +0530

    Optimized the Longest Repeating Character Replacement problem to a time complexity of O(N)

--- TwoPointers_SlidingWindow/longestRepeatingCharacterReplacement.java
+++ TwoPointers_SlidingWindow/longestRepeatingCharacterReplacement.java
@@ -5,7 +5,7 @@
 //brute force with time complexity => O(N²) and space complexity => O(1).
 package TwoPointers_SlidingWindow;
 import java.util.HashMap;
-class Solution {
+/*class Solution {
     public int characterReplacement(String s, int k) {
         int n = s.length();
         int maxLen = 0;
@@ -27,5 +27,31 @@ class Solution {
             }
        }
        return maxLen;
+    }
+}*/
+
+//optimal solution with time complexity => O(N) and space complexity => O(1).
+class Solution {
+    public int characterReplacement(String s, int k) {
+        HashMap <Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        int left = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        for(int right=0; right<n; right++){
+            char ch = s.charAt(right);
+
+            map.put(ch, map.getOrDefault(ch,0)+1);
+
+            maxFreq = Math.max(maxFreq, map.get(ch));
+
+            while( (right-left+1) - maxFreq > k){
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar)-1);
+                left++;
+            } 
+            maxLen = Math.max(maxLen, (right-left+1));      
+        }
+        return maxLen;
     }
 }
