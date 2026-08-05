fe25bca201d9cd80f5ad178557acbeed1be42c8a
Prashantfr <prashantiwari2005@gmail.com>
2026-08-05 12:07:53 +0530

    Optimized the Number of Substrings Containing All Three Characters problem to O(N) time complexity

--- TwoPointers_SlidingWindow/subStringContainingAllThreeCharacters.java
+++ TwoPointers_SlidingWindow/subStringContainingAllThreeCharacters.java
@@ -3,7 +3,7 @@
 
 //brute force solution with time complexity of O(n^2) and space complexity O(1).
 package TwoPointers_SlidingWindow;
-import java.util.HashSet;
+/*import java.util.HashSet;
 class Solution {
     public int numberOfSubstrings(String s) {
         int n = s.length();
@@ -16,6 +16,34 @@ class Solution {
                     count += (n-j);
                     break;
                 }
+            }
+        }
+        return count;
+    }
+}*/
+
+//optimal solution with a time complexity => O(N) and space complexity => O(1).
+import java.util.HashMap;
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int left = 0;
+        int count = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int right=0; right<n; right++){
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch,0)+1);
+
+            while(map.size()==3){
+                count += (n-right);
+
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar)-1);
+
+                if(map.get(leftChar)==0){
+                    map.remove(leftChar);
+                }
+                left++;
             }
         }
         return count;
