3780a0c45c33b832f24e304e30469d44aa836215
Prashantfr <prashantiwari2005@gmail.com>
2026-08-06 15:50:27 +0530

    Optimized the Remove Outermost Parentheses problem

--- Strings/removeOutermostParantheses.java
+++ Strings/removeOutermostParantheses.java
@@ -4,7 +4,7 @@
 //brute force solution with time complexity of O(n^2)(substring creation) and space complexity O(n).
 package Strings;
 
-class Solution {
+/*class Solution {
     public String removeOuterParentheses(String s) {
         StringBuilder result = new StringBuilder();
         int balance = 0;
@@ -16,6 +16,26 @@ class Solution {
             if(balance == 0){
                 result.append(s.substring(start+1, i));
                 start = i=1;
+            }
+        }
+        return result.toString();
+    }
+}*/
+
+//optimal solution with time complexity => O(N) and space complexity => O(1)(excludiing output).
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int depth = 0;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='('){
+                if(depth>0) result.append(ch);
+                depth++;
+            }
+                else {
+                    depth--;
+                    if(depth>0) result.append(ch);
             }
         }
         return result.toString();
