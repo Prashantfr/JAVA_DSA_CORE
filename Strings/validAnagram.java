9a6d360119d7e12446ec2dd4059a48c570122918
Prashantfr <prashantiwari2005@gmail.com>
2026-08-11 18:02:06 +0530

    Optimized the valid anagram problem to O(n) time complexity and O(1) space complexity

--- Strings/validAnagram.java
+++ Strings/validAnagram.java
@@ -1,10 +1,10 @@
-//problem name : Largest Odd Number in String
-//problem link : https://leetcode.com/problems/largest-odd-number-in-string/description/
+//problem name : Valid anagram.
+//problem link : https://leetcode.com/problems/valid-anagram/
 
 //brute force solution with time complexity of O(nlogn) and space complexity O(n).
 
 package Strings;
-import java.util.Arrays;
+/*import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
@@ -16,5 +16,25 @@ class Solution {
         Arrays.sort(b);
 
         return Arrays.equals(a,b);
+    }
+}
+*/
+
+//optimal solution with time complexity of O(n) and space complexity O(1).
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] freq = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+
+        for(int i=0; i<freq.length; i++){
+            if(freq[i] != 0) return false;
+        }
+        return true;
     }
 }
