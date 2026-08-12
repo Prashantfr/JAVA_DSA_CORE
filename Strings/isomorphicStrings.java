5a862d856d11087f980b689b265c0f649a4662bf
Prashantfr <prashantiwari2005@gmail.com>
2026-08-13 01:47:48 +0530

    Optimized the Isomorphic strings to O(n) time complexity

--- Strings/isomorphicStrings.java
+++ Strings/isomorphicStrings.java
@@ -4,7 +4,7 @@
 //brute force solution with time complexity of O(n^2) and space complexity O(1).
 
 package Strings;
-class Solution {
+/*class Solution {
     public boolean isIsomorphic(String s, String t) {
         int n = s.length();
 
@@ -20,6 +20,27 @@ class Solution {
                 if (t.charAt(j) == c2 && s.charAt(j) != c1)
                     return false;
             }
+        }
+        return true;
+    }
+}*/
+
+//optimal solution with time complexity of O(n) and space complexity O(1).
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for(int i=0; i<s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if(mapS[c1] != mapT[c2]){
+                return false;
+            }
+            mapS[c1] = i+1;
+            mapT[c2] = i+1;
         }
         return true;
     }
