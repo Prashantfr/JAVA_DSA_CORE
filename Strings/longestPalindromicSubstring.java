e1a22744493eba1c65f42c119cc8c4b16258b7c5
Prashantfr <prashantiwari2005@gmail.com>
2026-08-16 20:37:26 +0530

    Optimized the longest palindrome substring problem substring to O(n^2) time complexity

--- Strings/longestPalindromicSubstring.java
+++ Strings/longestPalindromicSubstring.java
@@ -4,7 +4,7 @@
 //brute force solution with time complexity of O(n^3) and space complexity O(1).
 package Strings;
 
-class Solution {
+/*class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
         String ans = "";
@@ -31,5 +31,37 @@ class Solution {
             right--;
         }
         return true;
+    }
+}
+*/
+
+//optimal solution with time complexity of O(n^2) and space complexity O(1).
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() < 1 ) return "";
+
+        int start = 0, end = 0;
+
+        for(int i=0; i<s.length(); i++){
+
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i+1);
+
+            int len = Math.max(len1, len2);
+
+            if(len > end - start){
+                start = i  - (len - 1)/2;
+                end = i + len/2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 }
