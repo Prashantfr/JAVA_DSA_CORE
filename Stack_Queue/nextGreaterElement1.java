75ffb9716183138006a410e57b39a5d9576a4441
Prashantfr <prashantiwari2005@gmail.com>
2026-08-18 13:02:04 +0530

    Solved the next greater element 1 problem with O(n * m) time complexity

---
+++ Stack_Queue/nextGreaterElement1.java
@@ -0,0 +1,31 @@
+//problem link : https://leetcode.com/problems/next-greater-element-i/description/
+//problem name : Next Greater Element I
+
+//brute force solution with time complexity of O(n * m) and space complexity O(1).
+//declare the j variable outside the second for loop so that the third loop can use the value of j.
+
+package Stack_Queue;
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+
+        for(int i=0; i<nums1.length; i++){
+            int target = nums1[i];
+            int j;
+
+            for(j=0; j<nums2.length; j++){
+                if(nums2[j] == target) break;
+            }
+
+            int nextGreater = -1;
+            for(int k=j+1; k<nums2.length; k++){
+                if(nums2[k] > target){
+                    nextGreater = nums2[k];
+                    break;
+                }
+            }
+            result[i] = nextGreater;
+        }
+        return result;
+    }
+}
