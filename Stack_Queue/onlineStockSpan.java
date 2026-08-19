9214e3f163bb52d7f3145a7a8a9a0ec05da887bb
Prashantfr <prashantiwari2005@gmail.com>
2026-08-19 17:00:08 +0530

    Optimized the online stock span problem to O(n) time complexity

--- Stack_Queue/onlineStockSpan.java
+++ Stack_Queue/onlineStockSpan.java
@@ -4,8 +4,7 @@
 //brute force solution with time complexity of O(n^2) and space complexity O(n).
 
 package Stack_Queue;
-
-import java.util.List;
+/*import java.util.List;
 import java.util.ArrayList;
 class StockSpanner {
     private List<Integer> prices;
@@ -25,6 +24,37 @@ class StockSpanner {
                 break;
             }
         }
+        return span;
+    }
+}*/
+
+//optimal solution with time complexity of O(n) and space complexity O(n).
+import java.util.*;
+class StockSpanner {
+    private List<Integer> prices;
+    private Stack<Integer> stack;
+
+    public StockSpanner() {
+        prices = new ArrayList<>();
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        prices.add(price);
+        int i = prices.size() - 1;
+
+        while(!stack.isEmpty() && prices.get(stack.peek()) <= price){
+            stack.pop();
+        }
+
+        int span;
+        if(stack.isEmpty()){
+            span = i + 1;
+        }
+        else{
+            span = i - stack.peek();
+        }
+        stack.push(i);
         return span;
     }
 }
