8c82ca50b974cf69c603b0ee3b7201efeaac7c49
Prashantfr <prashantiwari2005@gmail.com>
2026-08-19 19:22:56 +0530

    Solved the Sum of Subarray Ranges problem with O(n^2) time complexity

---
+++ Stack_Queue/sumOfSubarrayRanges.java
@@ -0,0 +1,27 @@
+//problem link : https://leetcode.com/problems/sum-of-subarray-ranges/description/
+//problem name : Sum of Subarray Ranges
+
+//brute force solution with time complexity of O(n^2) and space complexity O(1).
+
+
+package Stack_Queue;
+
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+
+        for(int i=0; i<n; i++){
+            int min = nums[i];
+            int max = nums[i];
+
+            for(int j=i; j<n; j++){
+                min = Math.min( min , nums[j] );
+                max = Math.max( max , nums[j] );
+
+                sum += (max - min);
+            }
+        }
+        return sum;
+    }
+}
