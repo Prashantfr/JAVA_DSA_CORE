0c69b6c928d0ce58fb7753d15862983c3d6d310a
Prashantfr <prashantiwari2005@gmail.com>
2026-08-19 20:22:08 +0530

    Optimized the Sum of Subarray Ranges problem to O(n) time complexity

--- Stack_Queue/sumOfSubarrayRanges.java
+++ Stack_Queue/sumOfSubarrayRanges.java
@@ -6,7 +6,7 @@
 
 package Stack_Queue;
 
-class Solution {
+/*class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
         long sum = 0;
@@ -21,6 +21,78 @@ class Solution {
 
                 sum += (max - min);
             }
+        }
+        return sum;
+    }
+}*/
+
+//optimal solution with time complexity of O(n) and space complexity O(n).
+import java.util.*;
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        return sumSubarrayMax(nums) - sumSubarrayMin(nums);
+    }
+
+    private long sumSubarrayMin(int[] nums){
+        int n = nums.length;
+        long sum = 0;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && nums[st.peek()] > nums[i]){
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? (i+1) : (i - st.peek());
+            st.push(i);
+        }
+
+        st.clear();
+
+        for(int i=n-1; i>=0; i--){
+            while(!st.isEmpty() && nums[st.peek()] >= nums[i]){
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? (n-i) : (st.peek() - i);
+            st.push(i);
+        }
+
+        for(int i=0; i<n; i++){
+            sum += (long) nums[i] * left[i] * right[i];
+        }
+        return sum;
+    } 
+
+    private long sumSubarrayMax(int[] nums){
+        int n = nums.length;
+        long sum = 0;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]){
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? (i+1) : (i - st.peek());
+            st.push(i);
+        }
+
+        st.clear();
+
+        for(int i=n-1; i>=0; i--){
+            while(!st.isEmpty() && nums[st.peek()] <= nums[i]){
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? (n-i) : (st.peek() - i);
+            st.push(i);
+        }
+
+        for(int i=0; i<n; i++){
+            sum += (long) nums[i] * left[i] * right[i];
         }
         return sum;
     }
