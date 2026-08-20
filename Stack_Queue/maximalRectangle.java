900d52971270f0595a9f64218b6338d9253244f1
Prashantfr <prashantiwari2005@gmail.com>
2026-08-20 19:36:08 +0530

    Optimized the maximal rectangle problem to O(nxm) time complexity

--- Stack_Queue/maximalRectangle.java
+++ Stack_Queue/maximalRectangle.java
@@ -5,7 +5,7 @@
 
 package Stack_Queue;
 
-class Solution {
+/*class Solution {
     public int maximalRectangle(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
@@ -30,6 +30,61 @@ class Solution {
             }
         }
 
+        return maxArea;
+    }
+}*/
+
+//optimal solution with time complexity of O(nxm) and space complexity O(m).
+import java.util.*;
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[] height = new int[m];
+        int maxArea = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                height[j] = (matrix[i][j] == '1') ? height[j]+1 : 0;
+            }
+            maxArea = Math.max(maxArea, largestRectangle(height));
+        }
+        return maxArea;
+    }
+
+    private int largestRectangle(int[] arr){
+        int n = arr.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? (i+1) : (i - st.peek());
+            st.push(i);
+        }
+
+        st.clear();
+
+        for(int i = n-1; i>=0; i--){
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]){
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? (n-i) : (st.peek() - i);
+            st.push(i);
+        }
+
+        int maxArea = 0;
+        for(int i=0; i<n; i++){
+            int width = left[i] + right[i] - 1;
+            int area = arr[i]*width;
+            maxArea = Math.max(area, maxArea);
+        }
         return maxArea;
     }
 }
