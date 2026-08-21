72684033d6f336c58137c97a169327b8546b6479
Prashantfr <prashantiwari2005@gmail.com>
2026-08-21 19:25:32 +0530

    Better solution for rainwater problem using O(n) time complexity and O(n) space complexity

--- Stack_Queue/trappingRainWater.java
+++ Stack_Queue/trappingRainWater.java
@@ -5,7 +5,7 @@
 
 package Stack_Queue;
 
-class Solution {
+/*class Solution {
     public int trap(int[] height) {
         int n = height.length;
         int water = 0;
@@ -21,6 +21,31 @@ class Solution {
             }
 
             water += Math.min(leftMax, rightMax) - height[i];
+        }
+        return water;
+    }
+}*/
+
+//better solution with time complexity of O(n) and space complexity O(n) using prefix and suffix.
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = height[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        }
+
+        rightMax[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        }
+
+        int water = 0;
+        for(int i=0; i<n; i++){
+            water += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
         return water;
     }
