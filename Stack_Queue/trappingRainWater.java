f308309af34dc1309182bc76851f0b064fbe2a6a
Prashantfr <prashantiwari2005@gmail.com>
2026-08-21 20:00:15 +0530

    Optimized the rainwater problem with O(n) time complexity and O(1) space complexity

--- Stack_Queue/trappingRainWater.java
+++ Stack_Queue/trappingRainWater.java
@@ -2,9 +2,7 @@
 //problem name : Trapping Rainwater
 
 //brute force solution with time complexity of O(n²) and space complexity O(1).
-
 package Stack_Queue;
-
 /*class Solution {
     public int trap(int[] height) {
         int n = height.length;
@@ -27,7 +25,7 @@ package Stack_Queue;
 }*/
 
 //better solution with time complexity of O(n) and space complexity O(n) using prefix and suffix.
-class Solution {
+/*class Solution {
     public int trap(int[] height) {
         int n = height.length;
         int[] leftMax = new int[n];
@@ -46,6 +44,39 @@ class Solution {
         int water = 0;
         for(int i=0; i<n; i++){
             water += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return water;
+    }
+}*/
+
+//optimal solution with time complexity of O(n) and space complexity O(1).
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int left = 0, right = height.length-1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while(left<right){
+            if(height[left] <= height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }
+                else{
+                    water += leftMax - height[left];
+                }
+                left++;
+            }
+
+            else{
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }
+                else{
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
         }
         return water;
     }
