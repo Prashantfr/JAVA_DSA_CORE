09898c2c9b226b73aa789772a859884f3d01d3e9
Prashantfr <prashantiwari2005@gmail.com>
2026-08-24 17:33:10 +0530

    Optimized the maximum sliding window problem with O(n) time complexity and O(k) space complexity

--- Stack_Queue/maxSlidingWindow.java
+++ Stack_Queue/maxSlidingWindow.java
@@ -5,7 +5,7 @@
 
 package Stack_Queue;
 
-class Solution {
+/*class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
         int[] res = new int[n-k+1];
@@ -20,4 +20,31 @@ class Solution {
         }
         return res;
     }
+}*/
+
+//optimal solution with time complexity of O(n) and space complexity O(k).
+import java.util.*;
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i=0; i<n; i++){
+
+            if(!dq.isEmpty() && dq.peekFirst() == i-k){
+                dq.pollFirst();
+            }
+
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            if(i>=k-1){
+                res[i-k+1] = nums[dq.peekFirst()];
+            }
+        }
+        return res;
+   }
 }
