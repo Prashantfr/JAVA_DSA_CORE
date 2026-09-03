1bd2117938d73abf834c39eb54cde3754ff7483b
Prashantfr <prashantiwari2005@gmail.com>
2026-09-03 12:12:21 +0530

    Optimized the Palindrome Linked List problem to O(1) space complexity and O(n) time complexity

--- Linked_List/PalindromeOrNotLL.java
+++ Linked_List/PalindromeOrNotLL.java
@@ -4,7 +4,7 @@
 //brute force solution with time complexity of O(N) and space complexity O(N).
 
 package Linked_List;
-import java.util.*;
+/*import java.util.*;
 class Solution {
     public boolean isPalindrome(ListNode head) {
         Stack<Integer> stack = new Stack<>();
@@ -22,5 +22,40 @@ class Solution {
             temp = temp.next;
         }
         return true;
+    }
+}*/
+
+//Optimal solution with time complexity of O(N) and space complexity O(1).
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverse(slow.next);
+
+        ListNode firstHalf = head;
+        while(secondHalf != null){
+            if(firstHalf.val != secondHalf.val) return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }    
+
+        private ListNode reverse(ListNode curr){
+            ListNode prev = null;
+            while(curr != null){
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+         }
+        return prev;
     }
 }
