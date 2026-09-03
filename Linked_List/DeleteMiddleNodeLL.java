ce401b5356d7491957d5ab1db29781477cf23faa
Prashantfr <prashantiwari2005@gmail.com>
2026-09-03 15:55:09 +0530

    Solved the Delete the Middle Node of a Linked List with O(n)(two passes) time complexity

---
+++ Linked_List/DeleteMiddleNodeLL.java
@@ -0,0 +1,29 @@
+//problem link : https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+//problem name : Delete the Middle Node of a Linked List
+
+//brute force solution with time complexity of O(N)(two passes) and space complexity O(1).
+
+package Linked_List;
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+
+        if(head == null || head.next==null){
+            return null;
+        }
+
+        int len = 0;
+        ListNode temp = head;
+        while(temp != null){
+            len++;
+            temp = temp.next;
+        }
+
+        temp = head;
+        for(int i=0; i<len/2-1; i++){
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        return head;
+    }
+}
