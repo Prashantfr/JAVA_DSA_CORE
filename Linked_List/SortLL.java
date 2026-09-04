660d69cb004441a14148b0034dc93e9bed44780d
Prashantfr <prashantiwari2005@gmail.com>
2026-09-04 22:07:34 +0530

    Optimized the Sort Linked List problem tp O(logn) space complexity

--- Linked_List/SortLL.java
+++ Linked_List/SortLL.java
@@ -4,7 +4,7 @@
 //brute force solution with time complexity of O(n log n) and space complexity O(n).
 
 package Linked_List;
-import java.util.*;
+/*import java.util.*;
 
 class Solution {
     public ListNode sortList(ListNode head) {
@@ -24,5 +24,54 @@ class Solution {
             curr = curr.next;
         }
         return head;
+    }
+}*/
+
+//Optimal solution with time complexity of O(n log n) and space complexity O(log n).
+//Using merge sort
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val<l2.val){
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+
+        if(l1 != null) temp.next = l1;
+        if(l2 != null) temp.next = l2;
+
+        return dummy.next;
     }
 }
