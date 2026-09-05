86d8cb99c513d107f572e9f66a87deb53d0dce65
Prashantfr <prashantiwari2005@gmail.com>
2026-09-05 11:15:06 +0530

    Optimized the Intersection of two Linked list problem with time complexity of O(N + M) and space complexity O(1)

--- Linked_List/IntersectionOfTwoLL.java
+++ Linked_List/IntersectionOfTwoLL.java
@@ -5,7 +5,7 @@
 
 package Linked_List;
 
-public class IntersectionOfTwoLL {
+/*public class IntersectionOfTwoLL {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
         ListNode a = headA;
@@ -24,5 +24,20 @@ public class IntersectionOfTwoLL {
         }
         
         return null; // no intersection
+    }
+}*/
+
+//Optimal solution with time complexity of O(N + M) and space complexity O(1).
+public class IntersectionOfTwoLL {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        while(p1 != p2){
+            p1 = (p1==null) ? headB : p1.next;
+            p2 = (p2==null) ? headA : p2.next;
+        }
+        return p1;
     }
 }
