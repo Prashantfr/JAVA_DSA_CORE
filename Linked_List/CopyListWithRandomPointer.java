dd28d16a20d1e06775082d22299af197959c587e
Prashantfr <prashantiwari2005@gmail.com>
2026-09-07 13:43:55 +0530

    Optimized the Copy List with Random Pointer problem to O(1) space complexity

--- Linked_List/CopyListWithRandomPointer.java
+++ Linked_List/CopyListWithRandomPointer.java
@@ -4,7 +4,7 @@
 //brute force solution with time complexity of O(n) and space complexity O(n).
 
 package Linked_List;
-import java.util.*;
+/*import java.util.*;
 
 class Solution{
 
@@ -40,5 +40,59 @@ class Solution{
             temp = temp.next;
         }
         return map.get(head);
+    }
+}*/
+
+//Optimal solution with time complexity of O(n) and space complexity O(1).
+class Solution {
+    class Node {
+    int val;
+    Node next;
+    Node random;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+  }
+    public Node copyRandomList(Node head) {
+
+        if (head == null) return null;
+
+        Node temp = head;
+
+        // Step 1: insert copy nodes
+        while (temp != null) {
+            Node copy = new Node(temp.val);
+            copy.next = temp.next;
+            temp.next = copy;
+            temp = copy.next;
+        }
+
+        // Step 2: random pointers
+        temp = head;
+        while (temp != null) {
+            if (temp.random != null) {
+                temp.next.random = temp.random.next;
+            }
+            temp = temp.next.next;
+        }
+
+        // Step 3: separate lists
+        Node dummy = new Node(0);
+        Node copyTail = dummy;
+        temp = head;
+
+        while (temp != null) {
+            Node copy = temp.next;
+            temp.next = copy.next;
+
+            copyTail.next = copy;
+            copyTail = copy;
+
+            temp = temp.next;
+        }
+
+        return dummy.next;
     }
 }
