168428d70387505de83b4700b923fbad6df627a7
Prashantfr <prashantiwari2005@gmail.com>
2026-09-15 18:58:18 +0530

    Optimized the Count Complete Tree Nodes problem to time complexity of O((log N)²) and space complexity O(logN)

--- Binary_Trees/CountCompleteTreeNodes.java
+++ Binary_Trees/CountCompleteTreeNodes.java
@@ -5,10 +5,44 @@
 
 package Binary_Trees;
 
-class Solution {
+/*class Solution {
     public int countNodes(TreeNode root) {
         if(root == null) return 0;
 
         return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}*/
+
+//Optimal solution with time complexity of O((log N)²)(height calculation O(log N) for each recursion) and space complexity O(logN).
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+
+        if(leftHeight == rightHeight){
+            return (1 << leftHeight) - 1;
+        }
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    private int getLeftHeight(TreeNode node){
+        int height = 0;
+        while(node != null){
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+
+    private int getRightHeight(TreeNode node){
+        int height = 0;
+        while(node != null){
+            height++;
+            node = node.right;
+        }
+        return height;
     }
 }
