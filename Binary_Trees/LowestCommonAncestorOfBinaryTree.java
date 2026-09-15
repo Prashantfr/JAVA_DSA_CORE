2a5c3e2e7be115c9d4b5da9fd63e70d78888f48a
Prashantfr <prashantiwari2005@gmail.com>
2026-09-15 17:32:52 +0530

    Solved the Lowest Common Ancestor of a Binary Tree problem using time complexity of O(n) and space complexity of O(h)

---
+++ Binary_Trees/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,23 @@
+//problem link : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+//problem name : Lowest Common Ancestor of a Binary Tree
+
+//Solution with time complexity of O(n) and space complexity O(h).
+
+package Binary_Trees;
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p , q);
+        TreeNode right = lowestCommonAncestor(root.right, p , q);
+
+        if(left != null && right != null){
+            return root;
+        }
+
+        return (left != null) ? left : right;
+    }
+}
