c4f7dc23f906c153aa19846cb151c2faf596e3f4
Prashantfr <prashantiwari2005@gmail.com>
2026-09-22 11:58:46 +0530

    Optimized the Construct Binary Search Tree from Preorder Traversal >>  problem to time complexity of O(n) and space complexity O(h)

--- Binary_Search_Tree/ConstructBSTfromPreorderTraversal.java
+++ Binary_Search_Tree/ConstructBSTfromPreorderTraversal.java
@@ -6,7 +6,7 @@
 
 package Binary_Search_Tree;
 
-class Solution {
+/*class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
         return build(preorder , Integer.MIN_VALUE , Integer.MAX_VALUE);
     }
@@ -24,6 +24,32 @@ class Solution {
         root.left = build(preorder , start+1 , i-1);
 
         root.right = build(preorder , i+1 , end);
+
+        return root;
+    }
+}*/
+
+//Optimal solution with time complexity of O(n) (each element used once) and space complexity O(h) (recursion (≈ O(log n) for balanced)).
+class Solution {
+    int i = 0;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder , Integer.MIN_VALUE , Integer.MAX_VALUE);
+    }
+
+    public TreeNode build(int[] preorder , int min , int max){
+
+        if(i >= preorder.length) return null;
+
+        int val = preorder[i];
+
+        if(val < min || val > max) return null;
+
+        TreeNode root = new TreeNode(val);
+        i++;
+
+        root.left = build(preorder , min , val);
+
+        root.right = build(preorder , val , max);
 
         return root;
     }
