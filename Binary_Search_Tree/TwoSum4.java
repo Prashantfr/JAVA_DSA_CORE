ee8b1627df186fc31b1daa331cf5703d941113d3
Prashantfr <prashantiwari2005@gmail.com>
2026-09-23 18:23:27 +0530

    Optimized the Two Sum IV - Input is a BST  problem to time complexity of O(n) and space complexity O(h)(height of tree)

--- Binary_Search_Tree/TwoSum4.java
+++ Binary_Search_Tree/TwoSum4.java
@@ -1,12 +1,12 @@
 //problem link : https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
 //problem name : Two Sum IV - Input is a BST
 
-//brute force solution with time complexity of O(n) and space complexity of O(n).
+//brute force solution with time complexity of O(n)(visit every node once) and space complexity of O(n)(hashset).
 
 package Binary_Search_Tree;
 import java.util.*;
 
-class Solution {
+/*class Solution {
 
     public boolean findTarget(TreeNode root, int k) {
         HashSet<Integer> set = new HashSet<>();
@@ -26,5 +26,60 @@ class Solution {
 
         // step 3: go left OR right
         return check(node.left, k, set) || check(node.right, k, set);
+    }
+}*/
+
+//Optimal solution with time complexity of O(n) and space complexity of O(h)(height of tree).
+//BSTiterator
+class BSTIterator {
+    Stack<TreeNode> stack = new Stack<>();
+    boolean reverse;
+
+    public BSTIterator(TreeNode root, boolean reverse) {
+        this.reverse = reverse;
+        pushAll(root);
+    }
+
+    public int next() {
+        TreeNode node = stack.pop();
+
+        if (reverse) pushAll(node.left);
+        else pushAll(node.right);
+
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    private void pushAll(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            if (reverse) node = node.right;
+            else node = node.left;
+        }
+    }
+}
+
+class Solution {
+
+    public boolean findTarget(TreeNode root, int k) {
+
+        if (root == null) return false;
+
+        BSTIterator left = new BSTIterator(root, false);   // smallest
+        BSTIterator right = new BSTIterator(root, true);   // largest
+
+        int i = left.next();
+        int j = right.next();
+
+        while (i < j) {
+            if (i + j == k) return true;
+            else if (i + j < k) i = left.next();
+            else j = right.next();
+        }
+
+        return false;
     }
 }
