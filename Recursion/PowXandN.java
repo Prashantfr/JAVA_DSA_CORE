3eb138e9c7141bf4ffeb4cb3c72189263b4c8f1c
Prashantfr <prashantiwari2005@gmail.com>
2026-09-24 12:06:15 +0530

    Optimized the Pow(x, n) problem to time complexity of O(logn) and space complexity O(logn)

--- Recursion/PowXandN.java
+++ Recursion/PowXandN.java
@@ -5,7 +5,7 @@
 
 package Recursion;
 
-class Solution {
+/*class Solution {
     public double myPow(double x, int n) {
         
         long power = n;
@@ -21,6 +21,29 @@ class Solution {
             result = result * x;
         }
 
+        return result;
+    }
+}*/
+
+//Optimal solution with time complexity of O(log n) and space complexity O(log n).
+class Solution {
+    public double myPow(double x, int n) {
+        long pow = n;
+
+        if(pow < 0){
+            x = 1/x;
+            pow = -pow;
+        }
+
+        double result = 1;
+
+        while(pow > 0){
+            if(pow % 2 == 1){
+                result = result * x;
+            }
+            x = x * x;
+            pow = pow/2;
+        }
         return result;
     }
 }
