22775c94d87b0deb327da14528173707afd810c0
Prashantfr <prashantiwari2005@gmail.com>
2026-09-25 17:53:29 +0530

    Solved the Count all subsequences with sum K problem with time complexity of O(2^n) and space complexity is O(n)

---
+++ Recursion/CountAllSubsequencesWithSumK.java
@@ -0,0 +1,24 @@
+//problem link : hhttps://takeuforward.org/practice/dsa/count-all-subsequences-with-sum-k
+//problem name : Count all subsequences with sum K
+
+//Solution with time complexity of O(2^n) and space complexity is O(n).
+
+package Recursion;
+
+class Solution {
+    public int countSubsequenceWithTargetSum(int[] nums, int k) {
+        return solve(0 , k , nums);
+    }
+
+    public int solve(int i , int target , int[] nums){
+        if(target == 0) return 1;
+
+        if(i == nums.length || target < 0) return 0;
+
+        int pick = solve(i+1 , target-nums[i] , nums);
+
+        int notPick = solve(i+1 , target , nums);
+
+        return pick + notPick;
+    }
+}
